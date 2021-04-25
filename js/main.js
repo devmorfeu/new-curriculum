ba43f5968f4f02945fa84a90bc7ae4270410e70d
morfeu9 <gabriel.nogueira21@hotmail.com>
2021-04-24 22:49:13 -0300

    js scroll page | melhoria no alinhamento das divs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 var beforeScroll = 0;
 var afterScroll;
 var $navbar = $('.navbar');
-  
+
+$(function() {
+  $.scrollify({
+    section : ".example-classname",
+    scrollSpeed: 20,
+    scrollbars: false
+  });
+});
+
 $(window).on("scroll", function(){
     
   afterScroll = $(this).scrollTop();
